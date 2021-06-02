3878b952449781ee4b2ba7137d4f187afe90cef3
xulingyue <57471671+xulingyue@users.noreply.github.com>
2021-06-02 21:01:12 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,29 +1,71 @@
 <?php
-/*
-* Boxmoe.com template file.
-*/
-get_header(); ?>
+/**
+ * The main template file.
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Mokore
+ */
+get_header();
+?>
+   
+   <?php if ( mokore_option('head_notice') != '0'){ 
+   		$text = mokore_option('notice_title');
+   	?>
+	<div class="notice">
+	   <i class="iconfont">&#xe66b;</i>
+	  <?php if(strlen($text) > 142 && !wp_is_mobile()){ ?> 
+	  	<marquee align="middle" behavior="scroll" loop="-1" scrollamount="6" style="margin: 0 8px 0 20px; display: block;" onMouseOut="this.start()" onMouseOver="this.stop()">
+			<div class="notice-content"><?php echo $text; ?></div>
+		</marquee>
+		<?php }else{ ?>
+			<div class="notice-content"><?php echo $text; ?></div>
+		<?php } ?>
+	</div>
+	<?php } ?>
+	
+	<?php 
+		if(mokore_option('top_feature')=='1'){
+			get_template_part('layouts/feature');
+		}
+	?>
+	
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">	
+		<h1 class="main-title">Posts</h1>
+		<?php
+		if ( have_posts() ) :
 
-      <section class="section section-about" id="about">
-        <div class="container">
-          <div class="section-head">
-            <span><?php if(is_home()) {echo'Home';}else{echo'category';}  ?></span>
-            <h2>New post</h2></div>
-			<div class="row justify-content-center align-items-center">
-<?php while ( have_posts() ) : the_post(); 
-if( boxmoe_com( 'bloglistrow' )== 'col-1') {
-echo'<div class="col-lg-12 col-md-12 post-boxmoe-standard">' ;	
-get_template_part( 'component/blog-list' );
-echo' </div>' ;	
- } else if (boxmoe_com( 'bloglistrow' )== 'col-2') { 
-get_template_part( 'component/blog-list-row' );
- }  else{ echo'<div class="col-lg-12 col-md-12 post-boxmoe-standard">' ;	get_template_part( 'component/blog-list' );echo' </div>' ;
- } ;
-endwhile; ?>
-           <div class="col-lg-12 col-md-12">
-             <?php bm_paging(); ?>
-			 </div>
-           </div>                     
-        </div>
-      </section>
-<?php get_footer(); ?>
+			if ( is_home() && ! is_front_page() ) : ?>
+			<header>
+				<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+			</header>
+
+			<?php
+			endif;
+			/* Start the Loop */
+			if(mokore_option('post_list_style') == 'standard'){
+				while ( have_posts() ) : the_post();
+				get_template_part( 'tpl/content', get_post_format() );
+				endwhile; 
+			}else{
+				get_template_part( 'tpl/content', 'thumb' );
+			}
+		?>
+		<?php else : get_template_part( 'tpl/content', 'none' ); endif; ?>
+		</main><!-- #main -->
+		<?php if ( mokore_option('pagenav_style') == 'ajax') { ?>
+		<div id="pagination"><?php next_posts_link('Previous'); ?></div>
+		<?php }else{ ?>
+		<nav class="navigator">
+		<?php previous_posts_link('<i class="iconfont">&#xe679;</i>') ?><?php next_posts_link('<i class="iconfont">&#xe6a3;</i>') ?>
+		</nav>
+		<?php } ?>
+	</div><!-- #primary -->
+<?php
+get_footer();
